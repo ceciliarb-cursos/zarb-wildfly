8f0c64aeb93efc6468570822b4b870414f44ef37
home <gerleymachado@gmail.com>
2019-06-12 11:18:08 -0300

    Primeiro commit

---
+++ teste/src/main/java/org/teste/model/Mapa.java
@@ -0,0 +1,121 @@
+package org.teste.model;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Version;
+
+@Entity
+public class Mapa implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	private static final long serialVersionUID = 1L;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	private String nome;
+
+	@Column
+	private String latitude;
+
+	@Column
+	private String longitude;
+
+	@Column
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Mapa)) {
+			return false;
+		}
+		Mapa other = (Mapa) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (nome != null && !nome.trim().isEmpty())
+			result += "nome: " + nome;
+		if (latitude != null && !latitude.trim().isEmpty())
+			result += ", latitude: " + latitude;
+		if (longitude != null && !longitude.trim().isEmpty())
+			result += ", longitude: " + longitude;
+		if (descricao != null && !descricao.trim().isEmpty())
+			result += ", descricao: " + descricao;
+		return result;
+	}
+}
